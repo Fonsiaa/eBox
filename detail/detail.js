8ac33d1abaae010ff0f047fe5a1503df518049fc
Fonsiaa <abeaalfonso@gmail.com>
2024-05-27 13:35:16 +0400

    detail

---
+++ detail/detail.js
@@ -0,0 +1,30 @@
+//navigational bar/menu bar
+const menu = document.querySelector('.menu')
+const screenMenu = document.querySelector('.screen-menu')
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  screenMenu.classList.toggle('active')
+})
+
+// for the details
+const allHoverImages = document.querySelectorAll('.hover-container div img');
+const imgContainer = document.querySelector('.img-container');
+
+window.addEventListener('DOMContentLoaded', () => {
+    allHoverImages[0].parentElement.classList.add('active');
+});
+
+allHoverImages.forEach((image) => {
+    image.addEventListener('mouseover', () =>{
+        imgContainer.querySelector('img').src = image.src;
+        resetActiveImg();
+        image.parentElement.classList.add('active');
+    });
+});
+
+function resetActiveImg(){
+    allHoverImages.forEach((img) => {
+        img.parentElement.classList.remove('active');
+    });
+}
